68d9a43ba1a34911f8c214d768e16d3db0d76164
Adrian Aguilar <132902881+adriiag8@users.noreply.github.com>
2024-06-11 05:21:10 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,8 +1,16 @@
 function skilsMember() {
-      var member = {
-    name: "Dicky",
-    age: 26,
-    skils: ["Javascript", "React", "Node.js"]
+        var member = {
+    name: "John",
+    age: 30,
+    skils: ["HTML", "CSS", "JS"],
+    showSkils: function () {
+      this.skils.forEach(function (skill) {
+        console.log(skill);
+      });
+    },
+  };
+  return member;
+}
   }
   return member;
 }
